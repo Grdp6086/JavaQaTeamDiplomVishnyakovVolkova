10fbff8520cafd8f2c53091249a0c0be8fdd7009
Dmitry Vishniakov <112121dfd@gmail.com>
2023-06-28 08:51:31 +0700

    Chahge SavingAccount

--- src/main/java/ru/netology/javaqadiplom/SavingAccount.java
+++ src/main/java/ru/netology/javaqadiplom/SavingAccount.java
@@ -25,6 +25,22 @@ public class SavingAccount extends Account {
               "Накопительная ставка не может быть отрицательной, а у вас: " + rate
             );
         }
+        if (minBalance < 0){
+            throw new IllegalArgumentException(
+                    "Минимальный баланс не может быть отрицательным, а у вас: " + minBalance
+            );
+        }
+        if (minBalance > maxBalance){
+            throw new IllegalArgumentException(
+                    "Минимальный баланс не может быть больше максимального, а у вас минимальный баланс: " + minBalance
+            );
+        }
+        if (initialBalance < minBalance){
+            throw new IllegalArgumentException(
+                    "Начальный баланс не может быть меньше минимального, а у вас начальный баланс: " + initialBalance
+            );
+
+        }
         this.balance = initialBalance;
         this.minBalance = minBalance;
         this.maxBalance = maxBalance;
@@ -45,8 +61,8 @@ public class SavingAccount extends Account {
         if (amount <= 0) {
             return false;
         }
-        balance = balance - amount;
-        if (balance > minBalance) {
+        if (balance - amount >= minBalance) {
+            balance -= amount;
             return true;
         } else {
             return false;
@@ -69,8 +85,8 @@ public class SavingAccount extends Account {
         if (amount <= 0) {
             return false;
         }
-        if (balance + amount < maxBalance) {
-            balance = amount;
+        if (balance + amount <= maxBalance) {
+            balance +=  amount;
             return true;
         } else {
             return false;
